9ffaa17135c4fed564e7ca20ac3f2f2f59afbeed
Janell Huyck <huyckjl@ucmail.uc.edu>
2026-02-06 21:28:26 -0500

    Add project files.

---
+++ WellnessScoreApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WellnessScoreApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WellnessScoreApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WellnessScoreApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
