ac398923bdd538789b52ea9ff69d0f00c4923d18
Janell Huyck <huyckjl@ucmail.uc.edu>
2026-02-06 22:09:09 -0500

    Gender selection working

--- WellnessScoreApp/MainPage.xaml.cs
+++ WellnessScoreApp/MainPage.xaml.cs
@@ -6,14 +6,33 @@
         public MainPage()
         {
             InitializeComponent();
+            SetGenderMale();
         }
 
         private void OnMaleTapped(object sender, EventArgs e)
         {
+            SetGenderMale();
+        }
+
+        private void SetGenderMale()
+        {
             selectedGender = "male";
             FrameMale.BorderColor = Colors.Blue;
             FrameFemale.BorderColor = Colors.Transparent;
+            MainScrollView.BackgroundColor = Colors.PowderBlue;
+        }
 
+        private void OnFemaleTapped(object sender, EventArgs e)
+        {
+            SetGenderFemale();
+        }
+
+        private void SetGenderFemale()
+        {
+            selectedGender = "male";
+            FrameMale.BorderColor = Colors.Transparent;
+            FrameFemale.BorderColor = Colors.HotPink;
+            MainScrollView.BackgroundColor = Colors.LightPink;
         }
     }
 }
